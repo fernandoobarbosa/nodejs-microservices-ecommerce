33783d154d194d396e2cae8135aa8c33ac311880
Fernando Barbosa <fernandobarbosa1697@gmail.com>
2023-05-18 11:38:46 -0300

    feat: 🎸 client routes

---
+++ client/src/infra/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { CreateClientController } from "../modules/create-client/create-client-controller";
+
+const router = Router();
+
+router.post("/clients", (req, res) => {
+    new CreateClientController().handle(req, res);
+})
+
+export {router}
